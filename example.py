4747d9eecebbc3ed8c97bef0dcf5fef70f0bf049
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-09-12 00:15:43 -0400

    Update example.py

--- example.py
+++ example.py
@@ -33,35 +33,6 @@ X_test_blur_2 = np.load('sample_data/X_test_blur_2.npy') # np.array([motion_blur
 X_test_blur_5 = np.load('sample_data/X_test_blur_5.npy') # np.array([motion_blur(x, size=5) for x in X_test])
 X_test_blur_15 = np.load('sample_data/X_test_blur_15.npy') #np.array([motion_blur(x, size=15) for x in X_test])
 
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.Flatten())
-# model.add(layers.Dense(64, activation='relu'))
-# model.add(layers.Dense(10))
-
-
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
-
-# model.fit(X_train, y_train, epochs=10,
-#           validation_data=(X_test, y_test))
-
-
-# yh = np.argmax(model.predict(X_test), axis=-1)
-# yh_blur_2 = np.argmax(model.predict(X_test_blur_2), axis=-1)
-# yh_blur_5 = np.argmax(model.predict(X_test_blur_5), axis=-1)
-# yh_blur_15 = np.argmax(model.predict(X_test_blur_15), axis=-1)
-
-
-# print('Original Accuracy: ' + str(np.sum(y_test == yh) / len(y_test)))
-# print('Blur (size=2) Accuracy: ' + str(np.sum(y_test == yh_blur_2) / len(y_test)))
-# print('Blur (size=5) Accuracy: ' + str(np.sum(y_test == yh_blur_5) / len(y_test)))
-# print('Blur (size=15) Accuracy: ' + str(np.sum(y_test == yh_blur_15) / len(y_test)))
 
 ohe = OneHotEncoder()
 nn = NearestNeighbors(n_neighbors=15)
@@ -91,23 +62,21 @@ ivis = Ivis(model=base_model, neighbour_matrix=neighbour_matrix, epochs=5)
 ivis.fit(X_train)
 
 embeddings_original = ivis.transform(X_test)
-embeddings_2 = ivis.transform(X_test_blur_2)
-embeddings_5 = ivis.transform(X_test_blur_5)
+# embeddings_2 = ivis.transform(X_test_blur_2)
+# embeddings_5 = ivis.transform(X_test_blur_5)
 embeddings_15 = ivis.transform(X_test_blur_15)
 
-p_2 = [None, None]
-p_5 = [None, None]
-p_15 = [None, None]
+# p_2 = [None, None]
+# p_5 = [None, None]
+# p_15 = [None, None]
 
-for i in range(2):
-    ks, p_2[i] = ks_2samp(embeddings_original[:, i], embeddings_2[:, i],
-                         alternative='two-sided', mode='asymp')
-    ks, p_5[i] = ks_2samp(embeddings_original[:, i], embeddings_5[:, i],
-                         alternative='two-sided', mode='asymp')
-    ks, p_15[i] = ks_2samp(embeddings_original[:, i], embeddings_15[:, i],
+# for i in range(2):
+    # ks, p_2[i] = ks_2samp(embeddings_original[:, i], embeddings_2[:, i],
+    #                      alternative='two-sided', mode='asymp')
+    # ks, p_5[i] = ks_2samp(embeddings_original[:, i], embeddings_5[:, i],
+    #                      alternative='two-sided', mode='asymp')
+ks, p_15 = ks_2samp(embeddings_original[:, 1], embeddings_15[:, 1],
                          alternative='two-sided', mode='asymp')
 
 
-print('Blur (size=2) K-S p=' + str(p_2))
-print('Blur (size=5) K-S p=' + str(p_5))
-print('Blur (size=15) K-S p=' + str(p_15))
+print(f"\n\n\nScore: {ks}\n\n\n")
